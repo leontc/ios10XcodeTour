959aa73c1a0ac8f4e443fbcce32d2742ac1aa693
LEON <>
2019-07-31 10:52:53 +1000

    initial commit

---
+++ XcodeTour/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  XcodeTour
+//
+//  Created by LEON ALVIS on 5/7/19.
+//  Copyright © 2019 LEON ALVIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
